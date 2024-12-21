15afdf1524227be13b76e9bec0fa387c711daf68
Namanskshetty <namanskshetty@gmail.com>
2024-12-21 16:22:21 +0530

    new tensor

--- main.py
+++ main.py
@@ -1,7 +1,60 @@
 import modules.speedtest as sp
+import cv2
 
-speed=sp.main()
+def Video():
+    print("insie")
+# Replace with your RTSP stream URL
+    rtsp_urls = ["rtsp://sterling:1KKDo&882v!jA!l0^N6Ox^b9%^r@192.168.1.38/stream1",
+                "rtsp://sterling:1KKDo&882v!jA!l0^N6Ox^b9%^r@192.168.1.35/stream1"]
 
+    # Open the RTSP streams
+    caps = [cv2.VideoCapture(url) for url in rtsp_urls]
+
+    # Check if all streams are opened successfully
+    if any(not cap.isOpened() for cap in caps):
+        print("Error: Could not open one or more RTSP streams.")
+        exit()
+    while True:
+        ret, frame = caps.read()
+        cv2.imshow(f'RTSP Stream {i}', frame)
+            #     # Press 'q' to exit the loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # # Define the codec and create VideoWriter objects
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # outs = [
+    #     cv2.VideoWriter(f'output{i}.avi', fourcc, 20.0, (int(caps[i].get(3)), int(caps[i].get(4)))) for i in range(len(caps))
+    # ]
+    #
+    # while True:
+    #     for i, cap in enumerate(caps):
+    #         ret, frame = cap.read()
+    #         if not ret:
+    #             print(f"Error: Failed to capture frame from stream {i}.")
+    #             continue
+    #
+    #         # Write the frame to the output file
+    #         outs[i].write(frame)
+    #
+    #         # Display the frame (optional)
+    #         cv2.imshow(f'RTSP Stream {i}', frame)
+    #
+    #     # Press 'q' to exit the loop
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+
+    # Release everything when done
+    for cap, out in zip(caps, outs):
+        cap.release()
+        out.release()
+    cv2.destroyAllWindows()
+
+
+
+
+print("Start..")
+# speed=sp.main()
+speed=50
 if speed<5:
     check=input("The speed is very slow!! ("+str(speed)+") Do you wish to continue Y/N. ")
     if check.lower()!='y':
@@ -11,3 +64,4 @@ if speed<5:
         print("Continue")
 else:
     print("pass")
+    Video()
